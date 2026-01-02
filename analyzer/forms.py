eea083832e2e2dd34fab852ae6b05f397825ec3a
lilajpoudel <112172349+lilajpoudel@users.noreply.github.com>
2026-01-02 22:35:43 +0545

    finished

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class SentimentForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea(attrs={'rows':4, 'cols':40}), label="Enter text")
+    text = forms.CharField(widget=forms.Textarea(attrs={'rows':4, 'cols':40}), label="Enter your opinion")
